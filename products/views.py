7be172ba4a7280a6d2d8718f010c5c595084538f
Howard Gill <theogill1369@gmail.com>
2022-08-24 22:41:04 -0400

    Modified products files in views.py + Created Products API in postman + created Get_All_products GET request

--- products/views.py
+++ products/views.py
@@ -1,10 +1,14 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
 
 @api_view[('GET')]
 def product_list(request):
-
-
-
-    return('ok')
+     products = Product.objects.all()
+     
+     serializer = ProductSerializer(products, many=True)
+     
+     
+     return Response(serializer.data)
